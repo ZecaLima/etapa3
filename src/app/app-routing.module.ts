9059b4b393e67aa810ca0a546cfc58e6bc244a38
vivianmartins <64610367+vivianmartins@users.noreply.github.com>
2021-05-16 20:14:50 +0100

    teste do slide e imagem do mesmo, altração das cores, criação da pagina de entrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'entrada',
     pathMatch: 'full'
   },
+ 
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'entrada',
+    loadChildren: () => import('./entrada/entrada.module').then( m => m.EntradaPageModule)
   }
+
 ];
 
 @NgModule({
---
+++ src/app/entrada/entrada.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { EntradaPageRoutingModule } from './entrada-routing.module';
+
+import { EntradaPage } from './entrada.page';
+import { ViewChild } from '@angular/core';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    EntradaPageRoutingModule
+  ],
+  declarations: [EntradaPage]
+})
+export class EntradaPageModule {}
