4109b7fe183a832148cf2432feb5723392d3644a
vivianmartins <64610367+vivianmartins@users.noreply.github.com>
2021-05-21 20:01:58 +0100

    login, ainda falta iniciar sessao

--- src/app/inicio/criarconta/criarconta.page.ts
+++ src/app/inicio/criarconta/criarconta.page.ts
@@ -9,13 +9,17 @@ import { NavController } from '@ionic/angular';
 export class CriarcontaPage implements OnInit {
 
   constructor(public nav: NavController) { }
-  
+
 
   ngOnInit() {
   }
 
  voltar(){
     this.nav.navigateForward('entrada');
+
+  }
+  login(){
+    this.nav.navigateForward('login');
 
   }
 }
