7c34def8ab43032132e880c5ba7bc4b1b3c1c311
ZecaLima <79462686+ZecaLima@users.noreply.github.com>
2021-05-30 18:19:41 +0100

    Pagina de favoritos Feita

--- src/app/favconta/favconta.page.ts
+++ src/app/favconta/favconta.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {  ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { IonSlides } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+import { CartService } from 'src/app/cart.service';
 
 @Component({
   selector: 'app-favconta',
@@ -7,9 +13,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavcontaPage implements OnInit {
 
-  constructor() { }
+  cart = [];
+  items = [];
+  toggled: boolean;
+  buttonColor: string;
+  isActives = false;
+  isActive = false;
+  rate = 0;
+  botaoestado = [
+    { state: false }
+  ];
+  botaofavorito = [
+    { state: false }
+  ];
+
+  sliderConfig = {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    centeredSlides: true
+  };
+  @ViewChild('slider', { static: true }) private slider:  IonSlides;
+
+  public slideOpts = {
+    initialSlide: 1,
+    speed: 400
+    };
+
+  public async ionSlideDidChange(): Promise<void> {
+    const index = await this.slider.getActiveIndex();
+  }
+
+  constructor(public nav: NavController,private cartService: CartService, private router: Router, public alertCtrl: AlertController) { }
+
+  async sharesuc() {
+    const alert = await this.alertCtrl.create({
+
+      subHeader: 'Partilhado com sucesso!',
+      backdropDismiss: false,
+      buttons: [{
+        text: 'Voltar',
+        handler: () => {
+
+        }
+      }
+      ]
+    });
+    await alert.present();
+  }
+
 
   ngOnInit() {
+    this.items = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+  }
+
+  criarcontas(){
+    this.nav.navigateForward('criarconta');
+
+  }
+  login(){
+    this.nav.navigateForward('login');
+
+  }
+  addToCart(product) {
+    this.cartService.addProduct(product);
+  }
+
+  openCart() {
+    this.router.navigate(['cart']);
+  }
+  pagefav(){
+    this.nav.navigateForward('fav');
+
   }
 
 }
